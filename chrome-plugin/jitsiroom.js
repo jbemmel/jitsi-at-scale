0d84361f4b7b0ec0824798db37b0e703643a932e
J vanBemmel <jvb127@gmail.com>
2021-05-01 12:06:34 -0500

    Update jitsiroom.js

--- chrome-plugin/jitsiroom.js
+++ chrome-plugin/jitsiroom.js
@@ -1,9 +1,9 @@
-let room = {
+let settings = {
   server: "beta.meet.jit.si",
   room:   "jitsi-at-scale"
 };
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set( room );
-  console.log(`Default Jitsi room set to ${room}`);
+  chrome.storage.sync.set( { settings } );
+  console.log(`Default Jitsi room set to ${settings}`);
 });
