e7410c15ee760104053b99f892cef623d04e26c8
J vanBemmel <jvb127@gmail.com>
2021-05-01 12:10:29 -0500

    Update popup.js

--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -1,24 +1,24 @@
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+// Get reference to 'start' button
+let startButton = document.getElementById("start");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+//chrome.storage.sync.get("color", ({ color }) => {
+//  changeColor.style.backgroundColor = color;
+//});
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+startButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: startStream,
   });
 });
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+function startStream() {
+  chrome.storage.sync.get("settings", ({ settings }) => {
+    console.log( `TODO: Find audio/video element, connect to room ${settings}, start streaming` );
   });
 }
